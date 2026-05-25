addc809098356d7f0ca8c8473ad931ac8c481a12
rogerinfas <contactorogeris@gmail.com>
2026-05-25 09:46:27 -0500

    feat(backend): integrate enrollment module into main application module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user.module';
 import { AuthModule } from './auth.module';
 import { AdmissionModule } from './admission.module';
+import { EnrollmentModule } from './enrollment.module';
 
 @Module({
-  imports: [UserModule, AuthModule, AdmissionModule],
+  imports: [UserModule, AuthModule, AdmissionModule, EnrollmentModule],
   controllers: [],
   providers: [],
 })
