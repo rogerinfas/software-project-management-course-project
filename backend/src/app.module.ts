2a5446c899ba1f380870e7c59f84e1e60e55228d
rogerinfas <contactorogeris@gmail.com>
2026-05-25 15:28:27 -0500

    feat(academic): register academic module in app module and create seed script

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,18 @@ import { UserModule } from './user.module';
 import { AuthModule } from './auth.module';
 import { AdmissionModule } from './admission.module';
 import { EnrollmentModule } from './enrollment.module';
+import { AcademicModule } from './academic.module';
 
 @Module({
-  imports: [UserModule, AuthModule, AdmissionModule, EnrollmentModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    AdmissionModule,
+    EnrollmentModule,
+    AcademicModule,
+  ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+
